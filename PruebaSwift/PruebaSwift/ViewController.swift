881b07d8179804dbbab29b399e71234cdae4debc
Guillermo Rosales <guillermo.rosales@globant.com>
2015-07-24 15:50:58 -0500

    Adoption process

---
+++ PruebaSwift/PruebaSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PruebaSwift
+//
+//  Created by Guillermo Rosales on 7/23/15.
+//  Copyright (c) 2015 Hackaton Petly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
